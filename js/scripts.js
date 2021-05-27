1fad6b0cb9c7b09ad001c30f98597fd4583601ae
Lucian <lucian.miller185@gmail.com>
2021-05-27 10:28:35 -0700

    It will output all checked boxes from the work transportation section

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
-  $("form#transportation_survey").submit(function(event) {
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
     event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      const workTransportationMode = $(this).val();
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
   });
 });
